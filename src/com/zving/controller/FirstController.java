00656296f91ad2c50adb4dd002563ce0d5e94b92
clover-liuyi <50126015+clover-liuyi@users.noreply.github.com>
2019-07-24 09:14:21 +0800

    Add files via upload

---
+++ src/com/zving/controller/FirstController.java
@@ -0,0 +1,9 @@
+package com.zving.controller;
+
+public class FirstController {
+
+	public static void main(String[] args) {
+		System.out.println("第一个控制器");
+	}
+
+}
---
+++ src/com/zving/controller/SecondController.java
@@ -0,0 +1,9 @@
+package com.zving.controller;
+
+public class SecondController {
+
+	public static void main(String[] args) {
+		System.out.println("第二个控制器");
+	}
+
+}
