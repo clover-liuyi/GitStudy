d26e85310dd7cbffa2823b636f48ec12c16f215c
clover-liuyi <50126015+clover-liuyi@users.noreply.github.com>
2019-07-24 10:12:00 +0800

    Add files via upload

---
+++ src/com/zving/entity/User.java
@@ -0,0 +1,28 @@
+package com.zving.entity;
+
+public class User {
+	
+	private int id;
+	private String name;
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
