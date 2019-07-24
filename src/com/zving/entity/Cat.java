3728e9160132d9118e4d21abe9fc75280ab287ad
clover-liuyi <banxia3@163.com>
2019-07-24 10:55:55 +0800

    猫

---
+++ src/com/zving/entity/Cat.java
@@ -0,0 +1,15 @@
+package com.zving.entity;
+
+public class Cat {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
